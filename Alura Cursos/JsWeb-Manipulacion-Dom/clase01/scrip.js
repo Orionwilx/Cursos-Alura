a450dcfcb9caa1c059df952cc46fc63070e2b490
Wilman Zuleta <wilman3d@hotmail.com>
2023-03-12 19:03:55 -0500

    Terminado clase 03 ocn un bug

--- Alura Cursos/JsWeb-Manipulacion-Dom/clase01/scrip.js
+++ Alura Cursos/JsWeb-Manipulacion-Dom/clase01/scrip.js
@@ -4,7 +4,24 @@ const btn = document.querySelector("[data-form-btn]"); //De esta forma se selecc
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = "";
+
+    //backticks (``) Sirve para mezclar codigo html con js
+    const content = `<div>
+                        <i class="far fa-check-square icon "></i>
+                        <span class="task">${value}</span>
+                    </div>`
+    console.log(content);
+    //La funcion innerHTML sirve apra agregar contenido al html desde js
+    task.innerHTML = content;
+
+    list.appendChild(task);
+
+    console.log(content);
 }
 
 console.log(btn);
