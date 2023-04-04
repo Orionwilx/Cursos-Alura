18fdb0b6d861bf1130281f4bdf8268f2a694feb6
Orion <wilman3d@hotmail.com>
2023-04-03 19:36:19 -0500

    Editar datos clase04

--- Alura Cursos/Js-web-CRUD con Js asincrono/1836-CRUD_JS_Async-proyecto_base/controller/client.controller.js
+++ Alura Cursos/Js-web-CRUD con Js asincrono/1836-CRUD_JS_Async-proyecto_base/controller/client.controller.js
@@ -1,15 +1,17 @@
-import { clientServices } from "../service/client-service.js";
+/* eslint-disable no-undef */
+/* eslint-disable n/handle-callback-err */
+import { clientServices } from '../service/client-service.js';
 
 const crearNuevaLinea = (nombre, email, id) => {
-    const linea = document.createElement("tr");
-    const contenido = `
+	const linea = document.createElement('tr');
+	const contenido = `
         <td class="td" data-td>${nombre}</td>
         <td>${email}</td>
         <td>
             <ul class="table__button-control">
                 <li>
                     <a
-                    href="../screens/editar_cliente.html"
+                    href="../screens/editar_cliente.html?id=${id}"
                     class="simple-button simple-button--edit"
                     >Editar</a
                     >
@@ -21,23 +23,28 @@ const crearNuevaLinea = (nombre, email, id) => {
                 </li>
             </ul>
         </td>`;
-    linea.innerHTML = contenido;
-    const btn = linea.querySelector("button");
-    btn.addEventListener("click", () => {
-        const id = btn.id;
-        clientServices.eliminarCliente(id).then( (respuesta) =>{
-            console.log(respuesta);
-        }).catch(err => alert("Ocurrio un error"));
-    })
-    return linea;
+	linea.innerHTML = contenido;
+	const btn = linea.querySelector('button');
+	btn.addEventListener('click', () => {
+		const id = btn.id;
+		clientServices
+			.eliminarCliente(id)
+			.then((respuesta) => {
+				console.log(respuesta);
+			})
+			.catch((err) => alert('Ocurrio un error'));
+	});
+	return linea;
 };
 
-const table = document.querySelector("[data-table]");
+const table = document.querySelector('[data-table]');
 
-clientServices.listaClientes().then( (data) =>{
-    data.forEach(({nombre, email, id}) => {
-        const nuevaLinea = crearNuevaLinea(nombre, email, id);
-        table.appendChild(nuevaLinea);
-    });
-}).catch( (error) => alert('Ocurrio un error'));
-
+clientServices
+	.listaClientes()
+	.then((data) => {
+		data.forEach(({ nombre, email, id }) => {
+			const nuevaLinea = crearNuevaLinea(nombre, email, id);
+			table.appendChild(nuevaLinea);
+		});
+	})
+	.catch((error) => alert('Ocurrio un error'));
